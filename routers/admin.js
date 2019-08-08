3eab027841597c80d9d020f195d0f63e1ea8a3e4
GouYuQing <3319948815@qq.com>
2019-08-08 18:54:46 +0800

    Classification management

--- routers/admin.js
+++ routers/admin.js
@@ -53,8 +53,29 @@ router.get('/user', function (req, res) {
 })
 /*分类管理 */
 router.get('/category',function(req,res){
-    res.render('admin/category_index', {
-        userInfo: req.userInfo
+    var page = Number(req.query.page || 1);
+    var limit = 3;
+    var pages = 0;
+    /**读取数据库中的总数据 */
+    Category.count().then(function (count) {
+        // console.log(count);
+        /*计算总页数 */
+        pages = Math.ceil(count / limit);
+        /**取值范围不能大于pages，不能小于1 */
+        page = Math.min(page, pages);
+        page = Math.max(page, 1);
+        var skip = (page - 1) * limit;
+
+    Category.find().limit(limit).skip(skip).then(function (categories) {
+            res.render('admin/category_index', {
+                userInfo: req.userInfo,
+                categories: categories,
+                page: page,
+                count:count,
+                limit:limit,
+                pages:pages
+            })
+        })
     })
 })
 /*添加分类 */
@@ -96,5 +117,90 @@ router.post('/category/add',function(req,res){
         url:'/admin/category'
        });
    })
+})
+/*分类修改 */
+router.get('/category/edit',function(req,res){
+    var id = req.query.id||' ';
+    Category.findOne({
+        _id:id
+    }).then(function(category){
+        if(!category){
+            res.render('admin/error',{
+                userInfo:req.userInfo,
+                message:'此信息不存在'
+            });
+        }else{
+            //存在及保存
+            res.render('admin/category_edit',{
+                userInfo:req.userInfo,
+                category:category
+            })
+        }
+    })
+})
+/*分类修改保存*/
+router.post('/category/edit',function(req,res){
+    var id = req.query.id||' ';
+    var name = req.body.name||' ';
+    Category.findOne({
+        _id:id
+    }).then(function(category){
+        if(!category){
+            res.render('admin/error',{
+                userInfo:req.userInfo,
+                message:'此信息不存在'
+            });
+            return Promise.reject();
+        }else{
+            //没做任何修改提交的时候
+           if(name == category.name){
+               res.render('admin/success',{
+                   userInfo:userInfo,
+                   message:'修改信息成功',
+                   url:'/admin/category'
+               });
+               return Promise.reject();
+           }else{
+            // 要修改的分类名称是否已经存在
+           return Category.findOne({
+                _id:{$ne:id},
+                name:name
+            })
+           }
+        }
+    }).then(function(sameCategory){
+        if(sameCategory){
+            res.render('admin/error',{
+                userInfo:req.userInfo,
+                message:'数据库中已经存在相同分类'
+            })
+            return Promise.reject();
+        }else{
+           return Category.update({
+                _id:id
+            },{
+                name:name
+            });
+        }
+    }).then(function(){
+        res.render('admin/success',{
+            userInfo:userInfo,
+            message:'修改信息成功',
+            url:'/admin/category'
+        });
+    })
+})
+/*分类删除 */
+router.get('/category/delete',function(req,res){
+    var id = req.query.id||' ';
+    Category.remove({
+        _id:id
+    }).then(function(){
+        res.render('admin/success',{
+            userInfo:userInfo,
+            message:'修改信息成功',
+            url:'/admin/category'
+        });
+    })
 })
 module.exports = router;
