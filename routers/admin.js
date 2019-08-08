a90f8f7c764e1c6962accc5d5b2724addb935ddd
GouYuQing <3319948815@qq.com>
2019-08-08 11:29:20 +0800

    The background page

--- routers/admin.js
+++ routers/admin.js
@@ -2,32 +2,51 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
 
-router.use(function(req,res,next){
-    if(!req.userInfo.isAdmin){
+router.use(function (req, res, next) {
+    if (!req.userInfo.isAdmin) {
         res.send('只有管理员才可以进入此页面');
         return;
     }
     next();
 })
 /*后台管理 */
-router.get('/',function(req,res,next){
-    res.render('admin/index',{
-        userInfo:req.userInfo
+router.get('/', function (req, res, next) {
+    res.render('admin/index', {
+        userInfo: req.userInfo
     })
 })
 /*用户管理 */
-router.get('/user',function(req,res){
-   /*限制获取的数据条数，展示的用户个数limit（number）
-   skip(2):忽略前两条数据
-   每页显示两条 1-2 忽略0
-   3-4 忽略2
-   */
+router.get('/user', function (req, res) {
+    /*限制获取的数据条数，展示的用户个数limit（number）
+    skip(2):忽略前两条数据
+    每页显示两条 1-2 忽略0
+    3-4 忽略2
+    */
     /*为了实现用户列表的展示，需要从数据库中读取所有的用户列表信息 */
-    User.find().limit(3).then(function(users){
-        res.render('admin/user_index',{
-            userInfo:req.userInfo,
-            users:users
+    var page = Number(req.query.page || 1);
+    var limit = 3;
+    var pages = 0;
+    /**读取数据库中的总数据 */
+    User.count().then(function (count) {
+        // console.log(count);
+        /*计算总页数 */
+        pages = Math.ceil(count / limit);
+        /**取值范围不能大于pages，不能小于1 */
+        page = Math.min(page, pages);
+        page = Math.max(page, 1);
+        var skip = (page - 1) * limit;
+
+        User.find().limit(limit).skip(skip).then(function (users) {
+            res.render('admin/user_index', {
+                userInfo: req.userInfo,
+                users: users,
+                page: page,
+                count:count,
+                limit:limit,
+                pages:pages
+            })
         })
     })
+
 })
 module.exports = router;
