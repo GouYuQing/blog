b27763eeb8375df5161d532b1e133db2ce3dcedc
GouYuQing <3319948815@qq.com>
2019-08-08 15:47:10 +0800

    Add the classification

--- routers/admin.js
+++ routers/admin.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var Category = require('../models/Category');
+
 
 router.use(function (req, res, next) {
     if (!req.userInfo.isAdmin) {
@@ -48,5 +50,51 @@ router.get('/user', function (req, res) {
         })
     })
 
+})
+/*分类管理 */
+router.get('/category',function(req,res){
+    res.render('admin/category_index', {
+        userInfo: req.userInfo
+    })
+})
+/*添加分类 */
+router.get('/category/add',function(req,res){
+    res.render('admin/category_add', {
+        userInfo: req.userInfo
+    })
+})
+/*分类的保存 */
+router.post('/category/add',function(req,res){
+   var name = req.body.name||' ';
+   if(name ==' '){
+       res.render('admin/error',{
+        userInfo: req.userInfo,
+        message:'名称不能为空'
+       });
+       return;
+   }
+   //验证数据库中是否有相同的分类名称
+   Category.findOne({
+       name:name
+   }).then(function(rs){
+       if(rs){
+        res.render('admin/error',{
+            userInfo: req.userInfo,
+            message:'分类信息已经存在'
+           })
+           return Promise.reject();
+       }else{
+           //不存在，保存
+           return new Category({
+               name:name
+           }).save();
+       }
+   }).then(function(newCategory){
+    res.render('admin/success',{
+        userInfo: req.userInfo,
+        message:'分类保存成功',
+        url:'/admin/category'
+       });
+   })
 })
 module.exports = router;
