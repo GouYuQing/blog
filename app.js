6cea27fdb4aa499cb8d38614d87a933882eb042a
GouYuQing <3319948815@qq.com>
2019-08-10 00:28:51 +0800

    Page beautification

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /*入口文件 */
-
 var express = require('express');
 var swig = require('swig');
 var mongoose = require('mongoose');
@@ -13,7 +12,6 @@ var User = require('./models/User');
 //设置静态文件托管
 app.use('/public',express.static(__dirname+'/public'));
 
-
 //定义模板引擎
 app.engine('html',swig.renderFile);
 //模板文件存放目录
@@ -26,7 +24,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(function(req,res,next){
     req.cookies = new Cookies(req,res);
     console.log(req.cookies.get('userInfo'));
-    
+
+
     //解析登录用户的cookies信息
     req.userInfo={};
     if(req.cookies.get('userInfo')){
@@ -51,9 +50,11 @@ app.use(function(req,res,next){
     }
 })
 
+
 app.use('/admin',require('./routers/admin'));
 app.use('/api',require('./routers/api'));
 app.use('/',require('./routers/main'));
+
 
 mongoose.connect('mongodb://localhost:27017/blog',{ useNewUrlParser: true },function(err){
     if(err){
